5554b34a8d9667dd50538d4f92e2e98e1af6580b
vibr <vibr@itu.dk>
2021-09-06 11:34:20 +0200

    Tested and implemented isLeapYear

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -3,11 +3,73 @@ using Xunit;
 
 namespace MyApp.Tests
 {
-    public class UnitTest1
+    public class LeapYearTest
     {
+    
+    LeapYearClass leapYearClass = new LeapYearClass();
+
         [Fact]
-        public void Test1()
+        public void divisble_by_four()
         {
+
+            //Arrange
+            bool testFour, testSeven;
+
+        
+            //Act
+            testFour = leapYearClass.isDivisbleByFour(4);
+            testSeven = leapYearClass.isDivisbleByFour(7);
+
+
+            //Assert
+            Assert.Equal(true, testFour);
+            Assert.Equal(false, testSeven);
+        }
+
+        public void divisble_by_hundred(){
+
+            //Arrange
+            bool testTwoHundred;
+
+
+            //Act
+            testTwoHundred = leapYearClass.isDivisbleByHundred(200);
+
+            //Assert
+            Assert.Equal(true, testTwoHundred);
+        }
+
+        public void divisble_by_fourHundred(){
+
+            //Arrange
+            bool testSixteenHundred, testSeventeenHundred;
+
+            //Act
+            testSixteenHundred = leapYearClass.isDivisbleByFourHundred(1600);
+            testSeventeenHundred = leapYearClass.isDivisbleByFourHundred(1700);
+
+            //Assert
+            Assert.Equal(true, testSixteenHundred);
+            Assert.Equal(false, testSeventeenHundred);
+
+        }
+
+        public void is_leap_year(){
+            
+            //Arrange
+            bool year2021, year2024, year1600, year1700;
+
+            //Act
+            year2021 = leapYearClass.isLeapYear(2021);
+            year2024 = leapYearClass.isLeapYear(2024);
+            year1600 = leapYearClass.isLeapYear(1600);
+            year1700 = leapYearClass.isLeapYear(1700);
+
+            //Assert
+            Assert.Equal(false, year2021);
+            Assert.Equal(true, year2024);
+            Assert.Equal(true, year1600);
+            Assert.Equal(false, year1700);
 
         }
     }
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -2,11 +2,52 @@
 
 namespace MyApp
 {
-    class Program
+    public class LeapYearClass
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+
+        public bool isDivisbleByFour(int year){
+            if (year % 4 == 0){
+                return true;
+            }else{
+                return false;
+            }
         }
+
+        public bool isDivisbleByHundred(int year){
+            if(year % 100 == 0){
+                return true;
+            }else{
+                return false;
+            }
+
+        }
+
+        public bool isDivisbleByFourHundred(int year){
+            if(year % 400 == 0){
+                return true; 
+            }else {
+                return false;
+            }
+        }
+
+        public bool isLeapYear(int year){
+            if (isDivisbleByFour(year) && !isDivisbleByHundred(year)){
+                return true;
+            }else if (isDivisbleByFourHundred(year) && !isDivisbleByHundred(year)){
+                return true;
+            }else{
+                return false;
+            }
+        }
+   
+    }
+
+    public class Program{
+        
+        public static void Main(string[] args){
+            LeapYearClass leapYearClass = new LeapYearClass();
+
+            
+        }        
     }
 }
